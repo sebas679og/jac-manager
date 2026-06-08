04325bc2f43d4bb17331b1cd044ec6d928b8e47e
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-08 18:23:08 -0500

    feat: se agregan todos los archivos de configuracion de spring-boot

---
+++ backend/src/test/java/com/api/jac/JacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
