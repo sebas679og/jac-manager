b6a46af2fae0596c9c3539e02d4b615cf34238f0
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-08 20:06:31 -0500

    style: se aplica estilo de codigo con spotless

--- backend/src/main/java/com/api/jac/JacApplication.java
+++ backend/src/main/java/com/api/jac/JacApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JacApplication {
 
-	static void main(String[] args) {
-		SpringApplication.run(JacApplication.class, args);
-	}
-
+  static void main(String[] args) {
+    SpringApplication.run(JacApplication.class, args);
+  }
 }
