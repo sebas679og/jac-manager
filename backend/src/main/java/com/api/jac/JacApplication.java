0fcf45ff0a9175fc43d49efb7797fed0d36a14cb
Sebastian Orjuela Giraldo <sebastianorjuelagiraldo@gmail.com>
2026-06-08 19:42:10 -0500

    feat: actualizacion de .class de aplicacion

--- backend/src/main/java/com/api/jac/JacApplication.java
+++ backend/src/main/java/com/api/jac/JacApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JacApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(JacApplication.class, args);
 	}
 
